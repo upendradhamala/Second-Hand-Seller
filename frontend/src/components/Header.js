b20f10919f8a5a11e87ea725a2d66335d0d3d74b
upendradhamaladev <upendradhamala@koizai.com>
2021-12-05 14:30:31 +0545

    Made name change

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>KinBechSaman.com</Navbar.Brand>
+            <Navbar.Brand>SecondHandSell</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
 
